95f2dcc9165a20803c90fc69cb620e861f7f0aa5
Jeremy Haile <jhaile@gmail.com>
2019-10-21 21:11:04 -0400

    Use body when scrolling to avoid exceeding max HTTP length with scroll_id

--- lib/chewy/search/scrolling.rb
+++ lib/chewy/search/scrolling.rb
@@ -39,7 +39,7 @@ module Chewy
           yield(hits) if hits.present?
           break if fetched >= total
           scroll_id = result['_scroll_id']
-          result = perform_scroll(scroll: scroll, scroll_id: scroll_id)
+          result = perform_scroll(scroll: scroll, body: {scroll_id: scroll_id})
         end
       end
 
